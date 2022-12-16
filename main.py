6a6d99daced02a34fe8e7bdf111629e71a77566d
Eshwar <reshwar2004@gmail.com>
2022-12-16 21:07:16 +0530

    Basic pygame screen set up using classes

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 import pygame
-
-WIDTH, HEIGHT = 900,500
-BG_COLOR = (255,255,255)
-FPS = 60
-
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-
-def draw_window():
-    WINDOW.fill(BG_COLOR)
-    pygame.display.update()
-
-def main():
-    run = True
-
-    clock = pygame.time.Clock()
+import random
+from enum import Enum
+class SnakeGame():
     
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+    def __init__(self):
 
-        draw_window()
-        pygame.display.set_caption(f'Current FPS: {str(clock.get_fps())}')
+        self.WIDTH, self.HEIGHT = 900,500
+        self.BG_COLOR = (255,255,255)
+        self.FPS = 60
 
-    pygame.quit()
+        self.DISPLAY = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.draw_window()
+
+    def draw_window(self):
+        self.DISPLAY.fill(self.BG_COLOR)
+        pygame.draw.rect(self.DISPLAY, (255,0,0), pygame.Rect(self.WIDTH/2 - 30 ,self.HEIGHT/2 - 30, 60,60))
+        pygame.display.update()
+
+    def game_loop(self):
+        run = True
+
+        clock = pygame.time.Clock()
+    
+        while run:
+            clock.tick(self.FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            self.draw_window()
+            pygame.display.set_caption(f'Current FPS: {str(clock.get_fps())}')
+
+        pygame.quit()
 
 if __name__ == "__main__":
-    main()
+
+    pygame.init()
+
+    game = SnakeGame()
+    game.game_loop()
+     
